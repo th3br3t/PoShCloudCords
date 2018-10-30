c867101c355e616c0404790e454f785b477b0fde
Unknown <th3br3t@gmail.com>
2018-10-30 08:45:23 -0700

    Corrected YAML file syntax

--- appveyor.yml.ps1
+++ appveyor.yml.ps1
@@ -2,7 +2,7 @@
 
 environment:
   NugetApiKey:
-    secure: RRnC6NwiccW4vW9257cp
+    secure: h0l79oAJkHZ7S4Qvcm1BdSuAQY1MU+Jj8dkcSZUV68E=
 
 # Allow WMF5 (i.e. PowerShellGallery functionality)
 os: WMF 5
@@ -10,7 +10,7 @@ os: WMF 5
 # Skip on updates to the readme.
 # We can force this by adding [skip ci] or [ci skip] anywhere in commit message 
 skip_commits:
-message: /updated readme.*|update readme.*s/
+  message: /updated readme.*|update readme.*s/
 
 build: false
 
