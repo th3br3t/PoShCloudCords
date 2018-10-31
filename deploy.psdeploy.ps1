782f5ce105f89aba026ef2c444e5825872c090a2
Unknown <th3br3t@gmail.com>
2018-10-31 11:05:53 -0700

    !deploy

--- deploy.psdeploy.ps1
+++ deploy.psdeploy.ps1
@@ -4,6 +4,24 @@ if(
     $env:BHBuildSystem -eq 'AppVeyor'
    )
 {
+    Deploy Master {
+        By AppVeyorModule {
+            FromSource $ENV:BHProjectName
+            To AppVeyor
+            WithOptions @{
+                Version = $env:APPVEYOR_BUILD_VERSION
+            }
+        }
+    }
+}
+
+<#
+# Publish to AppVeyor if we're in AppVeyor
+if(
+    $env:BHProjectName -and $ENV:BHProjectName.Count -eq 1 -and
+    $env:BHBuildSystem -eq 'AppVeyor'
+   )
+{
     Deploy Module {
         By AppVeyorModule {
             FromSource $ENV:BHProjectName
@@ -14,3 +32,4 @@ if(
         }
     }
 }
+#>
