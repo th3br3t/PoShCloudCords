58905d5bba2d189342438cb72f8a5c13d4a52c88
Unknown <th3br3t@gmail.com>
2018-10-31 12:05:42 -0700

    Finish testing

--- deploy.psdeploy.ps1
+++ deploy.psdeploy.ps1
@@ -9,27 +9,8 @@ if(
             FromSource $ENV:BHProjectName
             To Environment
             WithOptions @{
-                Name = 'CloudCords'
                 Version = $env:APPVEYOR_BUILD_VERSION
             }
         }
     }
 }
-<#
-# Publish to AppVeyor if we're in AppVeyor
-if(
-    $env:BHProjectName -and $ENV:BHProjectName.Count -eq 1 -and
-    $env:BHBuildSystem -eq 'AppVeyor'
-   )
-{
-    Deploy Module {
-        By AppVeyorModule {
-            FromSource $ENV:BHProjectName
-            To AppVeyor
-            WithOptions @{
-                Version = $env:APPVEYOR_BUILD_VERSION
-            }
-        }
-    }
-}
-#>
