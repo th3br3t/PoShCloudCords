d7abb9f03304a3f742be87a5b17c5fd44210da6f
Unknown <th3br3t@gmail.com>
2018-11-01 11:37:59 -0700

    !deploy

--- build.ps1
+++ build.ps1
@@ -1,13 +1,24 @@
-﻿param ($Task = 'Default')
+﻿<#
+.Description
+Installs and loads all the required modules for the build.
+Derived from scripts written by Warren F. (RamblingCookieMonster)
+#>
+
+[cmdletbinding()]
+param ($Task = 'Default')
+"Starting build"
 
 # Grab nuget bits, install modules, set build variables, start build.
+"  Install Dependent Modules"
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Install-Module Psake, PSDeploy, BuildHelpers -force
-Install-Module Pester -Force -SkipPublisherCheck
-Import-Module Psake, BuildHelpers
+Install-Module Psake, PSDeploy, BuildHelpers, PSScriptAnalyzer, PlatyPS -force -Scope CurrentUser
+Install-Module Pester -MinimumVersion 4.1, PowerShellGet -Force -SkipPublisherCheck -Scope CurrentUser -AllowClobber
 
-Set-BuildEnvironment
+"  Import Dependent Modules"
+Import-Module InvokeBuild, BuildHelpers, PSScriptAnalyzer
 
-Invoke-psake -buildFile .\psake.ps1 -taskList $Task -nologo
+Set-BuildEnvironment -ErrorAction SilentlyContinue
+
+Invoke-psake -buildFile $ENV:BHProjectPath\psake.ps1 -taskList $Task -nologo
 exit ( [int]( -not $psake.build_success ) )
