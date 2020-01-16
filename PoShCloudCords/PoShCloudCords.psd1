0448d794f8e86602baf24a113126cab66c7d5fe9
Bret Robinson <bret.robinson@kiran.com>
2020-01-16 09:42:08 -0800

    !deploy

--- PoShCloudCords/PoShCloudCords.psd1
+++ PoShCloudCords/PoShCloudCords.psd1
@@ -12,7 +12,7 @@
 RootModule = 'PoShCloudCords.psm1'
 
 # Version number of this module.
-ModuleVersion = '1.3.2.11'
+ModuleVersion = '1.4.1'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
--- build.ps1
+++ build.ps1
@@ -10,10 +10,19 @@ Import-Module Psake, BuildHelpers
 Set-BuildEnvironment
 
 #Update Module Manifest version
-[System.Version]$currentManifestVersion = (Import-LocalizedData -FileName PoShCloudCords.psd1).ModuleVersion
+[System.Version]$currentManifestVersion = (Import-LocalizedData -FileName .\PoShCloudCords\PoShCloudCords.psd1).ModuleVersion
 if ($buildNumber -gt $currentManifestVersion) {
-    Update-ModuleManifest -Path .\PoShCloudCords.psd1 -ModuleVersion $buildNumber
+    Update-ModuleManifest -Path .\PoShCloudCords\PoShCloudCords.psd1 -ModuleVersion $buildNumber
 }
+
+<#
+$manifest = Import-PowerShellDataFile .\PoShCloudCords\PoShCloudCords.psd1
+[version]$version = $Manifest.ModuleVersion
+# Add one to the build of the version number
+[version]$NewVersion = "{0}.{1}.{2}" -f $Version.Major, $Version.Minor, ($Version.Build + 1) 
+# Update the manifest file
+Update-ModuleManifest -Path .\PoShCloudCords.psd1 -ModuleVersion $NewVersion
+#>
 
 Invoke-psake -buildFile .\psake.ps1 -taskList $Task -nologo
 exit ( [int]( -not $psake.build_success ) )
