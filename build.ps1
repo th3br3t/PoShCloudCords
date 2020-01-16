0860b5d1d5479cd8bce01944d613868355fb67e3
Bret Robinson <bret.robinson@kiran.com>
2020-01-16 09:26:17 -0800

    !deploy

--- build.ps1
+++ build.ps1
@@ -9,5 +9,11 @@ Import-Module Psake, BuildHelpers
 
 Set-BuildEnvironment
 
+#Update Module Manifest version
+[System.Version]$currentManifestVersion = (Import-LocalizedData -FileName PoShCloudCords.psd1).ModuleVersion
+if ($buildNumber -gt $currentManifestVersion) {
+    Update-ModuleManifest -Path .\PoShCloudCords.psd1 -ModuleVersion $buildNumber
+}
+
 Invoke-psake -buildFile .\psake.ps1 -taskList $Task -nologo
 exit ( [int]( -not $psake.build_success ) )
